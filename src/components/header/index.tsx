4117ac206be0ff9466b29af1cf8dd56b4eb3e213
Matheus Figueiredo <matheus.figueiredo@br.ey.com>
2025-06-06 01:26:45 -0300

    tentando resolver problema

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <header className="w-full flex items-center justify-between py-4 px-8">
       <div className="flex gap-6">
-        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-none hover:bg-blue-500 hover:text-white transition">
+        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-0 hover:bg-blue-500 hover:text-white transition">
           Opção 1
         </button>
-        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-none hover:bg-blue-500 hover:text-white transition">
+        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-0 hover:bg-blue-500 hover:text-white transition">
           Opção 2
         </button>
       </div>
@@ -15,10 +15,10 @@ export default function Header() {
         Alcides e Mosinho
       </h1>
       <div className="flex gap-6">
-        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-none hover:bg-blue-500 hover:text-white transition">
+        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-0 hover:bg-blue-500 hover:text-white transition">
           Opção 3
         </button>
-        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-none hover:bg-blue-500 hover:text-white transition">
+        <button className="bg-transparent text-white font-semibold py-2 px-6 rounded underline underline-offset-4 decoration-[#7a582a] focus:outline-none border-0 hover:bg-blue-500 hover:text-white transition">
           Opção 4
         </button>
       </div>
