423f0570aa81366723203d2556e8cf61e7634a66
Matheus Figueiredo <matheus.figueiredo@br.ey.com>
2025-06-06 15:57:06 -0300

    voltando as modificacoes

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-white">
+        Você está na página de Home
+      </h1>
+    </div>
+  );
+}
