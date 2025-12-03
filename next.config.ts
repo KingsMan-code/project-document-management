811dd4c27ad3e8475bec46bcc0ab3c226cd64ddf
Lucas Figueiredo <lucasferreirafigueiredo@outlook.com>
2025-12-03 13:39:08 -0300

    ajustes para fazer o deploy no render como Webservice

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,23 @@ const repo = "project-document-management";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  basePath: isProduction ? `/${repo}` : '',
-  assetPrefix: isProduction ? `/${repo}` : '',
-  // basePath: '/project-document-management',
-  // assetPrefix: '/project-document-management/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+// const nextConfig: NextConfig = {
+//   basePath: isProduction ? `/${repo}` : '',
+//   assetPrefix: isProduction ? `/${repo}` : '',
+//   // basePath: '/project-document-management',
+//   // assetPrefix: '/project-document-management/',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   }
 
+// };
+
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
